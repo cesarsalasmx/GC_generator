005258eaad381441465beb35575dbbdf26ef12ec
Cesar Salas <contacto@cesarsalas.mx>
2024-05-26 03:49:30 -0600

    fixed: Add Middleware for lotes and giftcards

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::middleware('auth')->group(function () {
 
 Route::get('/activar', [PublicGiftcardController::class, 'showActivationForm'])->name('giftcards.showActivationForm');
 Route::post('/activar', [PublicGiftcardController::class, 'activate'])->name('giftcards.activate');
-Route::resource('lotes', LotesController::class);
-Route::resource('giftcards', GiftcardsController::class);
+Route::resource('lotes', LotesController::class)->middleware(['auth', 'verified']);
+Route::resource('giftcards', GiftcardsController::class)->middleware(['auth', 'verified']);
 Route::get('/export-pdf/{loteId}', [ExportController::class, 'exportPDF'])->name('export.pdf');
 require __DIR__.'/auth.php';
