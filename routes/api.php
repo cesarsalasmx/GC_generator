58d82406fd2d2d0a83c57ad37ad76d1db4076780
Cesar Salas <contacto@cesarsalas.mx>
2024-06-03 16:39:12 -0600

    Feat: Add authentication with token to API

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\GiftcardsController;
-
-Route::post('v1/giftcards/activate',[GiftcardsController::class ,'activate'])->name('v1.giftcards.activate');
+use App\Http\Middleware\VerifyApiToken;
+Route::post('v1/giftcards/activate',[GiftcardsController::class ,'activate'])
+    ->name('v1.giftcards.activate')
+    ->middleware(VerifyApiToken::class);
